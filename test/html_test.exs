6b9861b6db4419b9359bd56629d1f62fc30c518b
Nick Veys <nick@codelever.com>
2016-01-04 21:49:15 -0600

    Adding tests for fa_icon corner cases

--- test/html_test.exs
+++ test/html_test.exs
@@ -1,4 +1,47 @@
 defmodule FontAwesomePhoenix.HTMLTest do
   use ExUnit.Case, async: true
   doctest FontAwesomePhoenix.HTML
+  alias FontAwesomePhoenix.HTML
+
+  test "fa_icon w/name and classes with whitespace" do
+    result = HTML.fa_icon("camera-retro     4x", class: "other      another")
+    expected = [~s(<i class="fa fa-camera-retro fa-4x other another">), "", "</i>"]
+    assert {:safe, expected} == result
+  end
+
+  test "fa_icon w/nil class" do
+    result = HTML.fa_icon("usb", class: nil)
+    expected = [~s(<i class="fa fa-usb">), "", "</i>"]
+    assert {:safe, expected} == result
+  end
+
+  test "fa_icon w/empty class" do
+    result = HTML.fa_icon("usb", class: "")
+    expected = [~s(<i class="fa fa-usb">), "", "</i>"]
+    assert {:safe, expected} == result
+  end
+
+  test "fa_icon w/whitespace class" do
+    result = HTML.fa_icon("usb", class: "  ")
+    expected = [~s(<i class="fa fa-usb">), "", "</i>"]
+    assert {:safe, expected} == result
+  end
+
+  test "fa_icon w/nil text" do
+    result = HTML.fa_icon("usb", text: nil)
+    expected = [~s(<i class="fa fa-usb">), "", "</i>"]
+    assert {:safe, expected} == result
+  end
+
+  test "fa_icon w/empty text" do
+    result = HTML.fa_icon("usb", text: "")
+    expected = [~s(<i class="fa fa-usb">), "", "</i>"]
+    assert {:safe, expected} == result
+  end
+
+  test "fa_icon w/whitespace text" do
+    result = HTML.fa_icon("usb", text: "  ")
+    expected = [~s(<i class="fa fa-usb">), "", "</i>"]
+    assert {:safe, expected} == result
+  end
 end
