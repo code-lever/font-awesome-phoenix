2d6e9eb3f77076c84e621cd4d4b4898d9f05bebb
Nick Veys <nick@codelever.com>
2016-01-15 21:30:14 -0600

    Version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule FontAwesomePhoenix.Mixfile do
 
   def project do
     [app: :font_awesome_phoenix,
-     version: "0.2.1",
+     version: "0.3.0",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
