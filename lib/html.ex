a30f0f703538f9bfb4d49090b29c9bbc3aeddf7b
Nick Veys <nick@codelever.com>
2016-01-15 22:04:07 -0600

    Handling fa_icon with block and without opts

--- lib/html.ex
+++ lib/html.ex
@@ -35,6 +35,11 @@ defmodule FontAwesomePhoenix.HTML do
       iex> FontAwesomePhoenix.HTML.fa_icon("camera-retro 4x", class: "myclass")
       {:safe, ["<i class=\\"fa fa-camera-retro fa-4x myclass\\">", "", "</i>"]}
 
+      iex> FontAwesomePhoenix.HTML.fa_icon("at") do
+      ...>   EEx.eval_string("<%= \\"stuff\\" %>")
+      ...> end
+      {:safe, ["<i class=\\"fa fa-at\\">", "stuff", "</i>"]}
+
       iex> FontAwesomePhoenix.HTML.fa_icon("at", class: "x", data: [mood: :happy]) do
       ...>   Phoenix.HTML.Tag.content_tag(:em, "@")
       ...> end
@@ -45,10 +50,16 @@ defmodule FontAwesomePhoenix.HTML do
     data = Keyword.get(opts, :data, [])
     content_tag(:i, block, class: tag_class_string(names, opts), data: data)
   end
-  def fa_icon(names, opts \\ []) when is_binary(names) or is_list(names) do
+  def fa_icon(names, [do: block]) when is_binary(names) or is_list(names) do
+    content_tag(:i, block, class: tag_class_string(names))
+  end
+  def fa_icon(names, opts) when is_binary(names) or is_list(names) do
     data = Keyword.get(opts, :data, [])
     content_tag(:i, "", class: tag_class_string(names, opts), data: data)
     |> add_text(text(opts), align_tag(opts))
+  end
+  def fa_icon(names) when is_binary(names) or is_list(names) do
+    content_tag(:i, "", class: tag_class_string(names))
   end
 
   defp add_text({:safe, tag_strings}, text, align) do
@@ -80,7 +91,7 @@ defmodule FontAwesomePhoenix.HTML do
     |> Enum.map(&"fa-#{&1}")
   end
 
-  defp tag_class_string(names, opts) do
+  defp tag_class_string(names, opts \\ []) do
     (~w(fa) ++ fa_classes(names) ++ extra_classes(opts))
     |> Enum.map(&String.strip/1)
     |> Enum.join(" ")
