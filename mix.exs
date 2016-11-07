8f516926c81bcc0c601323e7a47a01c66f4545e7
Nick Veys <nick@codelever.com>
2016-11-06 22:26:31 -0600

    Update version to 1.0.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule FontAwesomePhoenix.Mixfile do
 
   def project do
     [app: :font_awesome_phoenix,
-     version: "0.4.0",
+     version: "1.0.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
